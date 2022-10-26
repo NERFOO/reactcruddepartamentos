f0852248fd1978a5762cf01260af7f5e59490004
NERFOO <james.jaime.1105@gmail.com>
2022-10-26 13:00:58 +0200

    DELETE CRUD

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import Departamentos from './components/Departamentos';
 import CreateDepartamento from './components/CreateDepartamento';
 import MenuDepartamentos from './components/MenuDepartamentos';
 import DetalleDepartamento from './components/DetalleDepartamento';
+import DeleteDepartamento from './components/DeleteDepartamento';
 
 export default class Router extends Component {
     render() {
@@ -12,7 +13,12 @@ export default class Router extends Component {
             var {num, nom, loc} = useParams();
 
             return(<DetalleDepartamento iddepartamento={num} nombre={nom} localidad={loc} />)
+        }
 
+        function DeleteDepartamentoElement() {
+            var { id } = useParams();
+
+            return(<DeleteDepartamento id={id} />)
         }
 
         return (<div>
@@ -22,6 +28,7 @@ export default class Router extends Component {
                     <Route path='/' element={<Departamentos />}/>
                     <Route path='/create' element={<CreateDepartamento />}/>
                     <Route path='/details/:num/:nom/:loc' element={<DetalleDepartamentoElement />}/>
+                    <Route path='/delete/:id' element={<DeleteDepartamentoElement />}/>
                 </Routes>
             </BrowserRouter>
 
--- src/components/DeleteDepartamento.js
+++ src/components/DeleteDepartamento.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Global from '../Global';
+import { Navigate } from 'react-router-dom';
 
 export default class DeleteDepartamento extends Component {
+
+    state = {
+        status : false
+    }
+
+    deleteDepartamento = (e) => {
+        e.preventDefault();
+
+        var numero = this.props.id;
+        var request = "/api/departamentos/" + numero;
+        var url = Global.urlDepartamentos + request;
+
+        axios.delete(url).then( res => {
+            this.setState({
+                status : true
+            });
+        })
+    }
+
     render() {
+        //TENEMOS QUE DIBUJAR AQUI EL H2
+        if(this.state.status == true) {
+            return (<Navigate to="/" />);
+        }
+
         return (<div>
-            <h1>Delete Departamento
-                <span style={{color:"red"}}>{this.props.id}</span>
+            <h1>¿Delete Departamento
+                <span style={{color:"red"}}> {this.props.id}</span>?
             </h1>
 
+            <form onSubmit={this.deleteDepartamento}>
+                <button className='btn btn-danger'>Eliminar departamento</button>
 
+            </form>
 
         </div>)
     }
